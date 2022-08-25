9c51889a82b87797d3f2aa71a9852259af44cae1
Michael Vargas <Michael.Vargas0290@gmail.com>
2022-08-25 10:53:31 -0500

    Add Routes nav components.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import './App.css';
+import { Form } from 'formik';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesDirectoryPage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='directory' element={<CampsitesDirectoryPage />} />
+      </Routes>
       <Footer />
     </div>
   );
